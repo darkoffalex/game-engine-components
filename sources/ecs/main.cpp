f52c481d05f3f5bf7cb72327aafa2717af5213c4
Alex Nem <darkoffalex@yandex.ru>
2024-07-14 15:26:08 +0300

    Initial commit

---
+++ sources/ecs/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/**
+ * Точка входа
+ * @param argc Кол-во аргументов
+ * @param argv Аргументы
+ * @return Код выполнения
+ */
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
